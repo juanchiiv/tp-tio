bc9aa811a0fffc3465cae28212833f92caa5b789
Lautaro Lamenza <laulamenza22@live.com.ar>
2022-06-20 19:10:14 -0300

    agregue archivo con las matematicas

---
+++ matematica.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Libreria con funciones matematicas
+ */
+
+function sumar($x, $y) {
+    $resultado = $x + $y;
+
+    return $resultado;
+}
+
+function restar($x, $y) {
+    $resultado = $x - $y;
+
+    return $resultado;
+}
+
+/**
+ * Multiplica los dos operandos
+ */
+function multiplicar($x, $y) {
+    $resultado = $x * $y;
+
+    return $resultado;
+}
+
+function dividir($x, $y) {
+    $resultado = $x / $y;
+
+    return $resultado;
+}
+
+function valorPi() {
+    return pi();
+}
